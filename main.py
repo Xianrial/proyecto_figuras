eec244906e07cb150acb5a58392a4655d897494a
Xian <xian.rial@rai.usc.es>
2026-09-16 19:17:34 +0200

    Agregadas nuevas funcionalidades al main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from lib import quadrado
 print("Projeto figuras")
+print(quadrado.get_identificador())
 lado=4
-print(f"A área dum quadrado de lado {lado} é: {quadrado.get_area(lado)}")
+print(f"A área dum {quadrado.get_identificador()} de lado {lado} é:"
+    f"{quadrado.get_area(lado)} e o perémetro é {quadrado.get_perimetro(lado)}")
